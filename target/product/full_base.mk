3b8378a2c73c77af25e14a1990d3ac7ba92d787b
scott <scott@ubuntu.com>
2014-12-19 01:00:44 -0500

    Build: slim up sounds and update (2/2)
    Change-Id: I06f44504fd2632e417b218a67d3e1eccfc76ed76
    Signed-off-by: scott <scott@ubuntu.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -29,7 +29,9 @@ PRODUCT_PACKAGES := \
 
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.com.android.dateformat=MM-dd-yyyy
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.config.ringtone=Triton.ogg \
+    ro.config.notification_sound=Io.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
@@ -38,7 +40,7 @@ PRODUCT_LOCALES := en_US
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudioSlim.mk)
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
